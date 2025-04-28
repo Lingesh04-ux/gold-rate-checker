e8fc51b55802cd5938afca27c643e11e21ce185a
Lingesh <111057785+Lingesh04-ux@users.noreply.github.com>
2025-04-28 10:27:00 +0530

    Update Datas.jsx

--- src/components/Datas.jsx
+++ src/components/Datas.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Datas = [{twentyFourKGold : 9831,
-     twentyTwoKGold : 9005,
-     eighteenKGold : 7373,
-     fourteenKGold : 5734,
+const Datas = [{twentyFourKGold : 9760,
+     twentyTwoKGold : 8940,
+     eighteenKGold : 7320,
+     fourteenKGold : 5693,
      platinum : 3339,
-     silver : 112,}]
+     silver : 111,}]
 
     // const twentyFourKGold = 9831;
     // const twentyTwoKGold = 9005;
